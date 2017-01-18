0ff3a2b56f5b35c9364f5533633751a985fc3089
Wonderbird <gatezh@users.noreply.github.com>
2017-01-18 17:06:18 -0500

    Update Binary Agents algorithm solution

--- Intermediate Algorithm Scripting/Binary Agents/Binary Agents (solution 1).js
+++ Intermediate Algorithm Scripting/Binary Agents/Binary Agents (solution 1).js
@@ -1,7 +1,6 @@
 
 function binaryAgent(str) {
-    str = str.split(" ").map((binary) => parseInt(binary, 2)).map((chrCode) => String.fromCharCode(chrCode)).join("");
-    return str;
+    return str.split(" ").map((binary) => parseInt(binary, 2)).map((chrCode) => String.fromCharCode(chrCode)).join("");
 }
 
 console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
