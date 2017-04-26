ca4dbbeb55a7fd96320f4ab409d6210ee762b5ce
Sergii Gatezh <gatezh@users.noreply.github.com>
2017-04-26 18:11:56 -0400

    Add buttons onclick events and digits display

--- Advanced Front End Development Projects/Build a JavaScript Calculator/script.js
+++ Advanced Front End Development Projects/Build a JavaScript Calculator/script.js
@@ -1,4 +1,13 @@
 
-console.log("linked");
+var display = document.getElementsByClassName("display")[0];
+// console.log(display);
+var input = [];
 
-
+function buttonClick(key) {
+    console.log(key);
+    var reNum = /\d/;
+    // var reOperator = /+/;
+    if (reNum.test(key)) {
+        display.innerHTML = key;
+    }
+}
