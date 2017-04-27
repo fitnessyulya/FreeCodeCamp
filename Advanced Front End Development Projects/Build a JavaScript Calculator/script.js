cd554d293a060323c5844361e0a044ff03b1fd47
Sergii Gatezh <gatezh@users.noreply.github.com>
2017-04-27 11:16:22 -0400

    Add digit input functionality

--- Advanced Front End Development Projects/Build a JavaScript Calculator/script.js
+++ Advanced Front End Development Projects/Build a JavaScript Calculator/script.js
@@ -1,13 +1,59 @@
 
 var display = document.getElementsByClassName("display")[0];
-// console.log(display);
-var input = [];
+var input = ["0"];
+
+function lastInput () {
+    return input[input.length-1];
+}
+
+function changeLastInput (key) {
+    if (key === "." && !/\./.test(lastInput())) {
+        console.log("if 1");
+        input[input.length-1] += key;
+    } else if (key === "." && /\./.test(lastInput())) {
+
+    // }
+    // else if (lastInput() === "0" && key === ".") {
+    //     console.log("if 1");
+    //     input[input.length-1] += key
+    } else if (lastInput() === "0") {
+        console.log("if 2");
+        input[input.length-1] = key;
+    } else if (/^\d+\.?\d*$/.test(lastInput())) {
+        console.log("if 3");
+        input[input.length-1] += key.toString();
+    }
+}
+
+function allClear () {
+    input = ["0"];
+}
+
+function displayInput () {
+    display.innerHTML = lastInput();
+}
 
 function buttonClick(key) {
     console.log(key);
+
     var reNum = /\d/;
     // var reOperator = /+/;
-    if (reNum.test(key)) {
-        display.innerHTML = key;
+
+    if (key == "AC") {
+        allClear();
     }
+
+    else if (reNum.test(key) || key == ".") {
+        console.log(reNum.test(lastInput()));
+
+        if (reNum.test(lastInput())) {
+            changeLastInput(key);
+            // lastInput = `${lastInput}${key}`;
+            // lastInput() += key.toString();
+        // } else { 
+        //     input.push(key);
+        }
+    }
+    console.log(input);
+    displayInput();
 }
