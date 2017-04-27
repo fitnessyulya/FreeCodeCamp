b1d3233f8f952e4cc89821821ecd8efe9868cd77
Sergii Gatezh <gatezh@users.noreply.github.com>
2017-04-27 17:56:15 -0400

    Add insertOperator(), change lastInput() return

--- Advanced Front End Development Projects/Build a JavaScript Calculator/script.js
+++ Advanced Front End Development Projects/Build a JavaScript Calculator/script.js
@@ -1,28 +1,42 @@
 
 var display = document.getElementsByClassName("display")[0];
 var input = ["0"];
+var reOperator = /[\/\*\-\+]/;
+var reNum = /\d/;
 
 function lastInput () {
-    return input[input.length-1];
+    return { val: input[input.length - 1],
+            indx: input.length - 1
+    }
 }
 
 function changeLastInput (key) {
-    if (key === "." && !/\./.test(lastInput())) {
+    if (key === "." && !/\./.test(lastInput().val)) {
         console.log("if 1");
         input[input.length-1] += key;
-    } else if (key === "." && /\./.test(lastInput())) {
-
-    // }
-    // else if (lastInput() === "0" && key === ".") {
-    //     console.log("if 1");
-    //     input[input.length-1] += key
-    } else if (lastInput() === "0") {
+    } else if (key === "." && /\./.test(lastInput().val)) {
+    } else if (lastInput().val === "0") {
         console.log("if 2");
         input[input.length-1] = key;
-    } else if (/^\d+\.?\d*$/.test(lastInput())) {
+    } else if (/^\d+\.?\d*$/.test(lastInput().val)) {
         console.log("if 3");
         input[input.length-1] += key.toString();
     }
+}
+
+function insertOperator(key) {
+    if (reNum.test(lastInput().val)) {
+        input.push(key);
+        console.log(`pushed ${key}`);
+    }
+    else if (lastInput().val !== key) {
+        console.log(`lastInput.val is ${lastInput().val}`)
+        console.log(`lastInput.indx is ${lastInput().indx}`);
+        input[lastInput().indx] = key;
+        // input.splice(lastInput().index, 1, key.toString());
+        console.log(`last input is ${input[lastInput.indx]}`);
+    }
+    console.log(`input after insertOperator is ${input}`);
 }
 
 function allClear () {
@@ -30,29 +44,28 @@ function allClear () {
 }
 
 function displayInput () {
-    display.innerHTML = lastInput();
+    if (reNum.test(lastInput().val)) {
+        display.innerHTML = lastInput().val;
+    }
 }
 
 function buttonClick(key) {
     console.log(key);
-
-    var reNum = /\d/;
-    // var reOperator = /+/;
 
     if (key == "AC") {
         allClear();
     }
 
     else if (reNum.test(key) || key == ".") {
-        console.log(reNum.test(lastInput()));
+        console.log(reNum.test(lastInput().val));
 
-        if (reNum.test(lastInput())) {
+        if (reNum.test(lastInput().val)) {
             changeLastInput(key);
-            // lastInput = `${lastInput}${key}`;
-            // lastInput() += key.toString();
-        // } else { 
-        //     input.push(key);
         }
+    }
+
+    else if (reOperator.test(key)) {
+        insertOperator(key);
     }
     console.log(input);
     displayInput();
