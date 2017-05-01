c5be11c38b9ec6ee11b2219506d995d2c1ddcba3
Sergii Gatezh <gatezh@users.noreply.github.com>
2017-05-01 11:44:41 -0400

    Clean debug console.logs

--- Advanced Front End Development Projects/Build a JavaScript Calculator/script.js
+++ Advanced Front End Development Projects/Build a JavaScript Calculator/script.js
@@ -18,14 +18,11 @@ const lastInput = function lastInput() {
 const changeLastInput = function changeLastInput(key) {
   (result) ? allClear() : '';
   if (key === '.' && !/\./.test(lastInput().val)) {
-    console.log('if 1');
     input[input.length - 1] += key;
   } else if (key === '.' && /\./.test(lastInput().val)) {
   } else if (lastInput().val === '0') {
-    console.log('if 2');
     input[input.length - 1] = key;
   } else if (/^\d+\.?\d*$/.test(lastInput().val)) {
-    console.log('if 3');
     input[input.length - 1] += key.toString();
   }
 };
@@ -39,20 +36,13 @@ const insertOperator = function insertOperator(key) {
   if (result) {
     input.splice(0, input.length, result);
     result = '';
-    console.log(`input after splice is ${input}`);
   }
 
   if (reNum.test(lastInput().val)) {
     input.push(key);
-    console.log(`pushed ${key}`);
   } else if (lastInput().val !== key) {
-    console.log(`lastInput.val is ${lastInput().val}`);
-    console.log(`lastInput.indx is ${lastInput().indx}`);
     input[lastInput().indx] = key;
-      // input.splice(lastInput().index, 1, key.toString());
-    console.log(`last input is ${input[lastInput.indx]}`);
   }
-  console.log(`input after insertOperator is ${input}`);
 };
 
 const nextInput = function nextInput(key) {
@@ -62,9 +52,7 @@ const nextInput = function nextInput(key) {
 const calculate = function calculate() {
   (reOperator.test(lastInput().val)) ? input.pop() : '';
   const inputString = input.reduce((acc, cur) => `${acc} ${cur}`);
-  console.log(`inputString is ${inputString}`);
   result = (Function(`return ${inputString}`)()).toString();
-  console.log(`result is ${result}`);
 };
 
 const displayLimit = function displayLimit() {
@@ -96,12 +84,10 @@ const displayInput = function displayInput() {
 };
 
 const buttonClick = function buttonClick(key) {
-  console.log(key);
 
   if (key === 'AC') {
     allClear();
   } else if (reNum.test(key) || key === '.') {
-    console.log(reNum.test(lastInput().val));
 
     if (reNum.test(lastInput().val)) {
       changeLastInput(key);
@@ -111,11 +97,8 @@ const buttonClick = function buttonClick(key) {
   } else if (reOperator.test(key)) {
     insertOperator(key);
   } else if (key === '=') {
-    console.log('about to calculate');
     calculate();
   }
 
-  console.log(input);
   displayInput();
-  console.log('after display run');
 };
