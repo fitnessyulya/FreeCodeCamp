5629077c816085ca0b49d7080b99812dc97c272c
Sergii Gatezh <gatezh@users.noreply.github.com>
2017-05-04 10:15:55 -0400

    Add change timers' length functions

--- Advanced Front End Development Projects/Build a Pomodoro Clock/app/js/script.js
+++ Advanced Front End Development Projects/Build a Pomodoro Clock/app/js/script.js
@@ -1,3 +1,20 @@
 
-console.log('linked');
-console.log('gulp test');
+let workLength = 25;
+let breakLength = 5;
+
+
+const changeWorkTime = function changeWorkTime (num) {
+  if (num < 0 && workLength > 1) {
+    workLength -= 1;
+  } else if (num > 0) {
+    workLength += 1;
+  }
+};
+
+const changeBreakTime = function changeBreakTime (num) {
+  if (num < 0 && breakLength > 1) {
+    breakLength -= 1;
+  } else if (num > 0) {
+    breakLength += 1;
+  }
+};
