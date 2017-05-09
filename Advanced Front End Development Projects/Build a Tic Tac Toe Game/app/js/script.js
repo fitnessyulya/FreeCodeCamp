282c88dfe55f195d233387c3d9e7946975694129
Sergii Gatezh <gatezh@users.noreply.github.com>
2017-05-09 17:53:32 -0400

    Add symbol choice

--- Advanced Front End Development Projects/Build a Tic Tac Toe Game/app/js/script.js
+++ Advanced Front End Development Projects/Build a Tic Tac Toe Game/app/js/script.js
@@ -1,8 +1,20 @@
-let playerSymbol = 'O';
-let computerSymbol = 'X';
+const symbol = document.querySelector('#symbol-select');
+const gameField = document.querySelector('#cells');
+
+let playerSymbol = '';
+let computerSymbol = '';
 let lastMoveSymbol = '';
 
-const gameField = document.querySelector('#cells');
+const chooseSymbol = function chooseSymbol(e) {
+  if (e.target !== e.currentTarget) {
+    const choice = e.target.textContent;
+    e.target.style.backgroundColor = 'yellow';
+    playerSymbol = (choice === 'X') ? 'X' : 'O';
+    computerSymbol = (playerSymbol === 'X') ? 'O' : 'X';
+    symbol.removeEventListener('click', chooseSymbol, false);
+    console.log(choice);
+  }
+};
 
 const playerMove = function playerMove(e) {
   if (e.target !== e.currentTarget) {
@@ -15,4 +27,5 @@ const playerMove = function playerMove(e) {
   e.stopPropagation();
 };
 
+symbol.addEventListener('click', chooseSymbol, false);
 gameField.addEventListener('click', playerMove, false);
