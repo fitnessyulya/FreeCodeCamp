ff1626dce756e4c3fba3d91a4214529023959f4f
Sergii Gatezh <gatezh@users.noreply.github.com>
2017-05-09 12:01:18 -0400

    Put mark in a cell on click

--- Advanced Front End Development Projects/Build a Tic Tac Toe Game/app/js/script.js
+++ Advanced Front End Development Projects/Build a Tic Tac Toe Game/app/js/script.js
@@ -1,2 +1,18 @@
+let playerSymbol = 'O';
+let computerSymbol = 'X';
+let lastMoveSymbol = '';
 
-console.log('js linked');
+const gameField = document.querySelector('#cells');
+
+const playerMove = function playerMove(e) {
+  if (e.target !== e.currentTarget) {
+    const clickedCell = e.target.id;
+    const cellToMark = document.querySelector(`#${clickedCell}`);
+    cellToMark.innerHTML = `<span>${playerSymbol}</span>`;
+    lastMoveSymbol = cellToMark.textContent;
+    console.log('clicked on ' + clickedCell);
+  }
+  e.stopPropagation();
+};
+
+gameField.addEventListener('click', playerMove, false);
