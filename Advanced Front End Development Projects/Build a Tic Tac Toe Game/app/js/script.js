18e8de0d33831682360bdd0a12ae27ab2dacb3f7
Sergii Gatezh <gatezh@users.noreply.github.com>
2017-05-10 17:51:45 -0400

    Add winDetector and resetGame functions

--- Advanced Front End Development Projects/Build a Tic Tac Toe Game/app/js/script.js
+++ Advanced Front End Development Projects/Build a Tic Tac Toe Game/app/js/script.js
@@ -1,10 +1,72 @@
-const symbol = document.querySelector('#symbol-select');
+const symbols = document.querySelector('#symbol-select');
 const gameField = document.querySelector('#cells');
+console.log(gameField);
 
 let playerSymbol = '';
 let computerSymbol = '';
 let lastMoveSymbol = '';
-const moves = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let winner = '';
+let moves = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+const resetGame = function resetGame() {
+  playerSymbol = '';
+  computerSymbol = '';
+  lastMoveSymbol = '';
+  winner = '';
+  moves = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  for (let child of gameField.children) {
+    child.innerHTML = '<span></span>';
+  }
+  for (let symbol of symbols.children) {
+    symbol.style = 'none';
+  }
+  symbols.addEventListener('click', chooseSymbol, false);
+  console.log('after reset');
+};
+
+const winDetector = function winDetector() {
+  const horizontal = [0, 1, 2];
+  const vertical = [0, 3, 6];
+  const diagonal = [0, 4, 8];
+
+  switch (true) {
+    case (moves[0] === moves[1]) && (moves[1] === moves[2]):
+      winner = moves[0];
+      alert(`${moves[0]} won!`);
+      break;
+    case (moves[3] === moves[4]) && (moves[4] === moves[5]):
+      winner = moves[3];
+      alert(`${moves[3]} won!`);
+      break;
+    case (moves[6] === moves[7]) && (moves[7] === moves[8]):
+      winner = moves[6];
+      alert(`${moves[6]} won!`);
+      break;
+    case (moves[0] === moves[3]) && (moves[3] === moves[6]):
+      winner = moves[0];
+      alert(`${moves[0]} won!`);
+      break;
+    case (moves[1] === moves[4]) && (moves[4] === moves[7]):
+      winner = moves[1];
+      alert(`${moves[1]} won!`);
+      break;
+    case (moves[2] === moves[5]) && (moves[5] === moves[8]):
+      winner = moves[2];
+      alert(`${moves[2]} won!`);
+      break;
+    case (moves[0] === moves[4]) && (moves[4] === moves[8]):
+      winner = moves[0];
+      alert(`${moves[0]} won!`);
+      break;
+    case (moves[2] === moves[4]) && (moves[4] === moves[6]):
+      winner = moves[2];
+      alert(`${moves[2]} won!`);
+      break;
+  }
+  if (winner) {
+    setTimeout(resetGame, 3000);
+  }
+};
 
 const firstMove = function firstMove() {
   const options = [0, 2, 6, 8];
@@ -19,16 +81,18 @@ const chooseSymbol = function chooseSymbol(e) {
     e.target.style.backgroundColor = 'yellow';
     playerSymbol = (choice === 'X') ? 'X' : 'O';
     computerSymbol = (playerSymbol === 'X') ? 'O' : 'X';
-    symbol.removeEventListener('click', chooseSymbol, false);
+    symbols.removeEventListener('click', chooseSymbol, false);
     console.log(choice);
   }
+  e.stopPropagation();
   if (computerSymbol === 'X') {
     firstMove();
   }
 };
 
 const playerMove = function playerMove(e) {
-  if (e.target !== e.currentTarget) {
+  if ((e.target !== e.currentTarget)
+  && playerSymbol) {
     const cellToMark = document.querySelector(`#${e.target.id}`);
     cellToMark.innerHTML = `<span>${playerSymbol}</span>`;
     lastMoveSymbol = cellToMark.textContent;
@@ -36,7 +100,8 @@ const playerMove = function playerMove(e) {
     console.log('clicked on ' + e.target.id);
   }
   e.stopPropagation();
+  winDetector();
 };
 
-symbol.addEventListener('click', chooseSymbol, false);
+symbols.addEventListener('click', chooseSymbol, false);
 gameField.addEventListener('click', playerMove, false);
