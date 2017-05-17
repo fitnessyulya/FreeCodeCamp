5054e42e9b58ec34b8b9906fa5427ea100929395
Sergii Gatezh <gatezh@users.noreply.github.com>
2017-05-17 17:47:04 -0400

    Update stylesheet structure and guplfile

--- Advanced Front End Development Projects/Build a Simon Game/gulpfile.js
+++ Advanced Front End Development Projects/Build a Simon Game/gulpfile.js
@@ -3,29 +3,29 @@
 // or
 // npm i gulp gulp-sass browser-sync --save-dev
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
   browserSync.init({
     server: {
       baseDir: 'app'
     },
-  })
-})
+  });
+});
 
 
 gulp.task('sass', function() {
-  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+  return gulp.src('app/scss/styles.scss')
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.reload({
       stream: true
-    }))
+    }));
 });
 
-gulp.task('watch', ['browserSync', 'sass'], function (){
+gulp.task('watch', ['browserSync', 'sass'], function () {
   gulp.watch('app/scss/**/*.scss', ['sass']);
   // Reloads the browser whenever HTML or JS files change
   gulp.watch('app/*.html', browserSync.reload);
