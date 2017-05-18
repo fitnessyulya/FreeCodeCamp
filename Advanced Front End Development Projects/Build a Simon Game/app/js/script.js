fc697c4a2da62b4e288440a0e4642d32c88b329e
Sergii Gatezh <gatezh@users.noreply.github.com>
2017-05-18 16:53:27 -0400

    Add on/off toggle

--- Advanced Front End Development Projects/Build a Simon Game/app/js/script.js
+++ Advanced Front End Development Projects/Build a Simon Game/app/js/script.js
@@ -1,2 +1,14 @@
 
-console.log('script is linked!');
+const onOffToggle = document.querySelector('#toggle-label');
+let gameOn = false;
+
+const gameSwitch = function gameSwitch() {
+  if (gameOn) {
+    gameOn = false;
+  } else {
+    gameOn = true;
+  }
+  console.log(gameOn);
+};
+
+onOffToggle.addEventListener('click', gameSwitch, false);
