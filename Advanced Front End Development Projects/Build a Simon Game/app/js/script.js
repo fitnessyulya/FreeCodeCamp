5d930a3f5af2762fe2f3b3fc1329b341c34d7a96
Sergii Gatezh <gatezh@users.noreply.github.com>
2017-05-19 11:06:15 -0400

    Add sound on playButton press

--- Advanced Front End Development Projects/Build a Simon Game/app/js/script.js
+++ Advanced Front End Development Projects/Build a Simon Game/app/js/script.js
@@ -1,14 +1,26 @@
 
+const playButtons = document.querySelector('.game');
 const onOffToggle = document.querySelector('#toggle-label');
 let gameOn = false;
 
 const gameSwitch = function gameSwitch() {
-  if (gameOn) {
-    gameOn = false;
-  } else {
-    gameOn = true;
-  }
-  console.log(gameOn);
+  gameOn = (gameOn) ? false : true;
+  console.log(`game switch is ${gameOn}`);
 };
 
+function play(sound) {
+  const audio = document.querySelector(`#audio-${sound}`);
+  if (audio) audio.play();
+}
+
+const pressPlayButton = function pressPlayButton(e) {
+  if (e.target !== e.currentTarget) {
+    const button = e.target.id;
+    console.log(`e.target.id is ${e.target.id}`);
+    play(button);
+  }
+  e.stopPropagation();
+};
+
+playButtons.addEventListener('mousedown', pressPlayButton, false);
 onOffToggle.addEventListener('click', gameSwitch, false);
