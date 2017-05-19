c14fffe51c93308890258239498fd2297a5f3590
Sergii Gatezh <gatezh@users.noreply.github.com>
2017-05-19 16:58:46 -0400

    Disable buttons by on/off switch

--- Advanced Front End Development Projects/Build a Simon Game/app/js/script.js
+++ Advanced Front End Development Projects/Build a Simon Game/app/js/script.js
@@ -1,16 +1,38 @@
 
 const playButtons = document.querySelector('.game');
 const onOffToggle = document.querySelector('#toggle-label');
+const strictMode = document.querySelector('#strict-status');
 let gameOn = false;
+let strict = false;
+let pattern = [];
+let count = pattern.length;
+
+const levelUp = function levelUp() {
+  const arr = ['green', 'red', 'yellow', 'blue'];
+  pattern.push(arr[Math.floor(Math.random() * arr.length)]);
+};
 
 const gameSwitch = function gameSwitch() {
   gameOn = (gameOn) ? false : true;
+  if (gameOn) {
+    playButtons.addEventListener('mousedown', pressPlayButton, false);
+    document.querySelectorAll('.play-button').forEach(button => button.classList.add('available'));
+    strictMode.classList.add('available');
+  } else {
+    playButtons.removeEventListener('mousedown', pressPlayButton, false);
+    document.querySelectorAll('.play-button').forEach(button => button.classList.remove('available'));
+    strictMode.classList.remove('available');
+  }
   console.log(`game switch is ${gameOn}`);
 };
 
 function play(sound) {
   const audio = document.querySelector(`#audio-${sound}`);
-  if (audio) audio.play();
+  if (audio) {
+    audio.play();
+    count += 1;
+    console.log(`count now is ${count}`);
+  }
 }
 
 const pressPlayButton = function pressPlayButton(e) {
@@ -22,5 +44,4 @@ const pressPlayButton = function pressPlayButton(e) {
   e.stopPropagation();
 };
 
-playButtons.addEventListener('mousedown', pressPlayButton, false);
 onOffToggle.addEventListener('click', gameSwitch, false);
